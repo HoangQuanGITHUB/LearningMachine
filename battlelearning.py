cc29eb3ec6b510d15604d4570ac48836bb11e101
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2023-10-09 19:44:11 +0700

    cac

--- battlelearning.py
+++ battlelearning.py
@@ -2,7 +2,7 @@ import rps, json
 from random import randint
 print("You can't win a battle against me >:)")
 
-klf=open('knowledge.json','r')
+klf=open('moves.json','r')
 movesf=klf.read()
 move=json.loads(movesf)['moves']
 
