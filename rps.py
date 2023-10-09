a0b0bbbabcfa6d7a7d243f2bb978fc9ac72e1d8e
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2023-10-09 19:43:06 +0700

    cac

---
+++ rps.py
@@ -0,0 +1,18 @@
+def play(uinput:int,cinput:int):
+    #1=rock
+    #2=paper
+    #3=scissors
+    if uinput==1 and cinput==2:
+        print('Computer wins!')
+    if uinput==2 and cinput==3:
+        print('Computer wins!')
+    if uinput==3 and cinput==1:
+        print('Computer wins!')
+    if cinput==1 and uinput==2:
+        print('User wins!')
+    if cinput==2 and uinput==3:
+        print('User wins!')
+    if cinput==3 and uinput==1:
+        print('User wins!')
+    if uinput==cinput:
+        print('Tie!')
